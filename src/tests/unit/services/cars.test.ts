83f5b251ae1450f6f3896a921f42309004d75fe6
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 18:34:32 -0300

    test: cria testes para método create do service de Cars

---
+++ src/tests/unit/services/cars.test.ts
@@ -0,0 +1,41 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import CarsModel from '../../../models/CarsModel';
+import CarsService from '../../../services/CarsService';
+import { carMock, carMockWithId } from '../../mocks/cars';
+import StatusCodes from '../../../interfaces/StatusCodes';
+const { expect } = chai;
+
+describe('Cars Service', () => {
+  const carsModel = new CarsModel();
+  const carsService = new CarsService(carsModel);
+
+  afterEach(async () => {
+    sinon.restore();
+  });
+
+  describe('Creating a car', () => {
+
+    before(async () => {
+      sinon.stub(carsModel, 'create').resolves(carMockWithId);
+    });
+
+    it('Succesfully created', async () => {
+      const createdCar = await carsService.create(carMock);
+      expect(createdCar).to.be.deep.equal({
+        code: StatusCodes.CREATED,
+        data: carMockWithId,
+      });
+    });
+
+    it('Semantic error', async () => {
+      try {
+        await carsService.create({ ...carMock, model: '' });
+      } catch (error: any) {
+        expect(error.message).to.be.equal('Should be at least 3 characters');
+      }
+    });
+
+  });
+
+});
