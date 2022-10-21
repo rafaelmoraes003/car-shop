ad7cf24981ee310e6a291d630db8842ce5892596
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 17:24:04 -0300

    feat: cria helper para validar ObjectId de documentos MongoDB

---
+++ src/utils/validateObjectId.ts
@@ -0,0 +1,21 @@
+import { isValidObjectId } from 'mongoose';
+import CustomError from '../interfaces/CustomError';
+import StatusCodes from '../interfaces/StatusCodes';
+
+const validateObjectId = (_id: string) => {
+  if (_id.length < 24) {
+    throw new CustomError(
+      'Id must have 24 hexadecimal characters',
+      StatusCodes.BAD_REQUEST,
+    );
+  }
+
+  if (!isValidObjectId(_id)) {
+    throw new CustomError(
+      'Object not found',
+      StatusCodes.NOT_FOUND,
+    );
+  }
+};
+
+export default validateObjectId;
