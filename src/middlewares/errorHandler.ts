82a3714f6a3688d2ca463f5b0d1fa91c3e184649
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 17:18:11 -0300

    feat: cria middleware para tratamento de erros

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import CustomError from '../interfaces/CustomError';
+import StatusCodes from '../interfaces/StatusCodes';
+
+const errorHandler = (
+  err: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  console.log(err.message);
+  if (!err.statusCode) {
+    return res.status(StatusCodes.SERVER_ERROR).json({
+      message: 'Server error.',
+    });
+  }
+  const { message, statusCode } = err;
+  return res.status(statusCode).json({ message });
+};
+
+export default errorHandler;
