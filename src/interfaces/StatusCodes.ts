e2b64fa496752dc3c130a34a9534100ca4827534
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 15:13:27 -0300

    feat: cria enum para status codes

---
+++ src/interfaces/StatusCodes.ts
@@ -0,0 +1,14 @@
+enum StatusCodes {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  SUCCESS_NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  SEMANTIC_ERROR = 422,
+  SERVER_ERROR = 500,
+}
+
+export default StatusCodes;
