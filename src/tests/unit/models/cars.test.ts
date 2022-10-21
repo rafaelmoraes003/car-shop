b837b24beac93192b9943c62d0cccdf751c87d8b
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 17:52:34 -0300

    test: cria testes para método create do model de Cars

---
+++ src/tests/unit/models/cars.test.ts
@@ -0,0 +1,28 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Model } from 'mongoose';
+import CarsModel from '../../../models/CarsModel';
+import { carMock, carMockWithId } from '../../mocks/cars';
+const { expect } = chai;
+
+describe('Cars Model', () => {
+  const carsModel = new CarsModel();
+
+  afterEach(async() => {
+    sinon.restore();
+  });
+
+  describe('Creating a car', () => {
+
+    before(async () => {
+      sinon.stub(Model, 'create').resolves(carMockWithId);
+    });
+
+    it('Succesfully created', async () => {
+      const createdCar = await carsModel.create(carMock);
+      expect(createdCar).to.be.deep.equal(carMockWithId);
+    });
+
+  });
+
+});
