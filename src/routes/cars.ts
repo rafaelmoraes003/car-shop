50238bc9fcc025f534cb108b3d9b150967cecc61
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 17:19:49 -0300

    feat: cria o enpoint POST /cars

---
+++ src/routes/cars.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import CarsController from '../controllers/CarsController';
+import CarsModel from '../models/CarsModel';
+import CarsService from '../services/CarsService';
+
+const cars = Router();
+const carsModel = new CarsModel();
+const carsService = new CarsService(carsModel);
+const carsController = new CarsController(carsService);
+
+cars.post('/', carsController.create);
+
+export default cars;
