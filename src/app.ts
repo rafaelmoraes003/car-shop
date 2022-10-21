83ee60cb5af95394fe229599b1c5b5b9eb18420a
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 17:14:29 -0300

    feat: implementa o endpoint /cars e o middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler';
+import carsRoute from './routes/cars';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carsRoute);
+
+app.use(errorHandler);
 
 export default app;
