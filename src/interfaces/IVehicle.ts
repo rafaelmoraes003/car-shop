d3cd0e1cf187956fb83289bd5ae52f9e942b7e9f
rafael moraes <rafaelsm003@gmail.com>
2022-10-21 14:53:14 -0300

    feat: cria schema zod de validação de corpo de requisição de veículos

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+const zodVehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
