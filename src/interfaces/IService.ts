ee5b4acc06c778ef08732ace3478e815ed4e4a18
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 02:43:08 -0300

    feat: cria método update na interface IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -9,6 +9,7 @@ interface IService<T> {
   create(obj: T): Promise<IServiceResponse<T>>,
   read(): Promise<IServiceResponse<T[]>>,
   readOne(_id: string): Promise<IServiceResponse<T>>
+  update(_id: string, obj: T): Promise<IServiceResponse<T>>,
 }
 
 export default IService;
