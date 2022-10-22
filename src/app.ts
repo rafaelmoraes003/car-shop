3f1bf684a6dc2427e364cd57564bb37f3320b476
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 14:25:29 -0300

    feat: cria a rota /motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import errorHandler from './middlewares/errorHandler';
 import carsRoute from './routes/cars';
+import motorcyclesRoute from './routes/motorcycles';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/cars', carsRoute);
+app.use('/motorcycles', motorcyclesRoute);
 
 app.use(errorHandler);
 
