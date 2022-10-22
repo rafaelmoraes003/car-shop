a198ab038e976f8add26d7905ee6a4372b74253d
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 14:29:30 -0300

    fix: cria condição no schema zod de motocicletas para aceitar a propriedade engineCapacity maior que 0 e menor ou igual a 2500

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
-const zodMotorcycleSchema = z.object({
+export const zodMotorcycleSchema = z.object({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().int().lte(2500),
+  engineCapacity: z.number().int().gt(0).lte(2500),
 });
 
 export type IMotorcycle = z.infer<typeof zodMotorcycleSchema> & IVehicle;
