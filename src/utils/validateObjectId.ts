be198e9b35304a2de8f5d8910dd2b6639b2c2d7f
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 01:42:19 -0300

    refactor: helper validateObjectId faz apenas validação do objectId e não mais do tamanho da string

--- src/utils/validateObjectId.ts
+++ src/utils/validateObjectId.ts
@@ -3,17 +3,10 @@ import CustomError from '../interfaces/CustomError';
 import StatusCodes from '../interfaces/StatusCodes';
 
 const validateObjectId = (_id: string): void => {
-  if (_id.length < 24) {
+  if (!isValidObjectId(_id)) {
     throw new CustomError(
       'Id must have 24 hexadecimal characters',
       StatusCodes.BAD_REQUEST,
-    );
-  }
-
-  if (!isValidObjectId(_id)) {
-    throw new CustomError(
-      'Object not found',
-      StatusCodes.NOT_FOUND,
     );
   }
 };
