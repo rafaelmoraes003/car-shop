6f3dd9e3faff3d15b5b08ec6a68102b77c5da791
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 14:28:05 -0300

    feat: cria o método update na classe MotorcyclesController para atualizar uma motocicleta baseada em seu _id

--- src/controllers/MotorcyclesController.ts
+++ src/controllers/MotorcyclesController.ts
@@ -39,6 +39,20 @@ class MotorcyclesController {
       next(error);
     }
   };
+
+  public update = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const { model, year, color, status, buyValue, category, engineCapacity } = req.body;
+    try {
+      const { code, data } = await this._motorcycleService.update(
+        id,
+        { model, year, color, status, buyValue, category, engineCapacity },
+      );
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default MotorcyclesController;
