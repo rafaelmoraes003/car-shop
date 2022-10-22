c15b38a82af380c2ca769ce27f021536f45ffd5b
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 01:39:46 -0300

    feat: cria método na classe CarsService para listar um carro baseado em seu _id

--- src/services/CarsService.ts
+++ src/services/CarsService.ts
@@ -1,9 +1,11 @@
+import CustomError from '../interfaces/CustomError';
 import { ICar, zodCarSchema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 import IService from '../interfaces/IService';
 import { zodVehicleSchema } from '../interfaces/IVehicle';
 import StatusCodes from '../interfaces/StatusCodes';
 import validateBody from '../utils/validateBody';
+import validateObjectId from '../utils/validateObjectId';
 
 class CarsService implements IService<ICar> {
   private _carModel: IModel<ICar>;
@@ -21,6 +23,13 @@ class CarsService implements IService<ICar> {
   public async read() {
     const cars = await this._carModel.read();
     return { code: StatusCodes.OK, data: cars };
+  }
+
+  public async readOne(_id: string) {
+    validateObjectId(_id);
+    const car = await this._carModel.readOne(_id);
+    if (!car) throw new CustomError('Object not found', StatusCodes.NOT_FOUND);
+    return { code: StatusCodes.OK, data: car };
   }
 }
 
