0fcb9e2d8aaaf9562675b9a58ea468bbf0a8d236
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 14:26:37 -0300

    feat: cria o método create na classe MotorcyclesController para criação de motocicleta

---
+++ src/controllers/MotorcyclesController.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import IService from '../interfaces/IService';
+
+class MotorcyclesController {
+  private _motorcycleService: IService<IMotorcycle>;
+
+  constructor(service: IService<IMotorcycle>) {
+    this._motorcycleService = service;
+  }
+
+  public create = async (req: Request, res: Response, next: NextFunction) => {
+    const { model, year, color, status, buyValue, category, engineCapacity } = req.body;
+    try {
+      const { code, data } = await this._motorcycleService.create({
+        model, year, color, status, buyValue, category, engineCapacity,
+      });
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default MotorcyclesController;
