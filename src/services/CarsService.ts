1fc563f4c5c34bd2ffd329d6735dd2645d454880
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 03:28:18 -0300

    feat: cria método delete na classe CarsService para deletar carro

--- src/services/CarsService.ts
+++ src/services/CarsService.ts
@@ -7,6 +7,8 @@ import StatusCodes from '../interfaces/StatusCodes';
 import validateBody from '../utils/validateBody';
 import validateObjectId from '../utils/validateObjectId';
 
+const objectNotFoundMessage = 'Object not found';
+
 class CarsService implements IService<ICar> {
   private _carModel: IModel<ICar>;
 
@@ -28,7 +30,7 @@ class CarsService implements IService<ICar> {
   public async readOne(_id: string) {
     validateObjectId(_id);
     const car = await this._carModel.readOne(_id);
-    if (!car) throw new CustomError('Object not found', StatusCodes.NOT_FOUND);
+    if (!car) throw new CustomError(objectNotFoundMessage, StatusCodes.NOT_FOUND);
     return { code: StatusCodes.OK, data: car };
   }
 
@@ -36,8 +38,15 @@ class CarsService implements IService<ICar> {
     validateBody(obj, zodCarSchema, zodVehicleSchema);
     validateObjectId(_id);
     const updatedCar = await this._carModel.update(_id, obj);
-    if (!updatedCar) throw new CustomError('Object not found', StatusCodes.NOT_FOUND);
+    if (!updatedCar) throw new CustomError(objectNotFoundMessage, StatusCodes.NOT_FOUND);
     return { code: 200, data: { _id, ...obj } };
+  }
+
+  public async delete(_id: string) {
+    validateObjectId(_id);
+    const deletedCar = await this._carModel.delete(_id);
+    if (!deletedCar) throw new CustomError(objectNotFoundMessage, StatusCodes.NOT_FOUND);
+    return { code: StatusCodes.SUCCESS_NO_CONTENT };
   }
 }
 
