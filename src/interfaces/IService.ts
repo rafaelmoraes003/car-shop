f2013fbe53785a430703c9acef252235389ad810
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 01:38:13 -0300

    feat: adiciona método readOne na interface IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -2,13 +2,13 @@ import StatusCodes from './StatusCodes';
 
 interface IServiceResponse<T> {
   code: StatusCodes,
-  data?: T,
-  error?: string,
+  data: T,
 }
 
 interface IService<T> {
   create(obj: T): Promise<IServiceResponse<T>>,
   read(): Promise<IServiceResponse<T[]>>,
+  readOne(_id: string): Promise<IServiceResponse<T>>
 }
 
 export default IService;
