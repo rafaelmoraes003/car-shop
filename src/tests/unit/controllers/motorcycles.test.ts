46fe43b2e801ccb5a18886c092e4768465dd33a4
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 21:59:00 -0300

    test: cria teste para método create da classe MotorcyclesController

---
+++ src/tests/unit/controllers/motorcycles.test.ts
@@ -0,0 +1,56 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { NextFunction, Request, Response } from 'express';
+import StatusCodes from '../../../interfaces/StatusCodes';
+import MotorcyclesModel from '../../../models/MotorcyclesModel';
+import MotorcyclesService from '../../../services/MotorcyclesService';
+import MotorcyclesController from '../../../controllers/MotorcyclesController';
+import { motorcycleMock ,motorcycleMockWithId, updatedMotorcycleMock, updatedMotorcycleMockWithId } from '../../mocks/motorcycles';
+import { IMotorcycle } from '../../../interfaces/IMotorcycle';
+const { expect } = chai;
+
+describe('Motorcycles Controller', () => {
+  const motorcyclesModel = new MotorcyclesModel();
+  const motorcyclesService = new MotorcyclesService(motorcyclesModel);
+  const motorcyclesController = new MotorcyclesController(motorcyclesService);
+
+  const serverError = Error('Server error.');
+
+  const req = {} as Request;
+  const res = {} as Response;
+  let next = new Function
+
+  after(async () => {
+    sinon.restore();
+  });
+
+  describe('Creating a motorcycle', () => {
+    before(async () => {
+      sinon.stub(motorcyclesService, 'create')
+      .onCall(0).resolves({
+        code: StatusCodes.CREATED,
+        data: motorcycleMockWithId as IMotorcycle,
+      })
+      .onCall(1).throws(serverError)
+
+      req.body = motorcycleMock;
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      next = sinon.stub();
+    });
+
+    it('Successfully created', async () => {
+      await motorcyclesController.create(req, res, next as NextFunction);
+      expect((res.status as sinon.SinonStub).calledWith(StatusCodes.CREATED)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(motorcycleMockWithId)).to.be.true;
+    });
+
+    it('Server error', async () => {
+      await motorcyclesController.create(req, res, next as NextFunction);
+      expect((next as sinon.SinonStub).calledWith(serverError)).to.be.true;
+    });
+
+  });
+
+
+});
