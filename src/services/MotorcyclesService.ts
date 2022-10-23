25a6b7a9a2f4754dc75df1a4e2817e5f3096c6bc
rafael moraes <rafaelsm003@gmail.com>
2022-10-22 21:51:49 -0300

    refactor: classe MotorcyclesService passa a ter atributo model do tipo DatabaseModel<IMotorcycle>

--- src/services/MotorcyclesService.ts
+++ src/services/MotorcyclesService.ts
@@ -1,11 +1,9 @@
 import { IMotorcycle, zodMotorcycleSchema } from '../interfaces/IMotorcycle';
-import MotorcyclesModel from '../models/MotorcyclesModel';
+import DatabaseModel from '../models/DatabaseModel';
 import APIService from './APIService';
 
-const motorcyclesModel = new MotorcyclesModel();
-
 class MotorcyclesService extends APIService<IMotorcycle> {
-  constructor(model = motorcyclesModel, schema = zodMotorcycleSchema) {
+  constructor(model: DatabaseModel<IMotorcycle>, schema = zodMotorcycleSchema) {
     super(model, schema);
   }
 }
